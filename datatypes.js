fabb70a008eb929f001b11602cad3f1051f13b65
kashish567 <kashishbhanushali7@gmail.com>
2024-02-21 20:07:11 +0530

    function

--- datatypes.js
+++ datatypes.js
@@ -34,3 +34,26 @@ console.log(Object);
 //Array
 let num = [1,2,4,5];
 console.log(num);
+
+//function
+function animal(name){
+    console.log("This is " + name + "!");
+}
+animal("horse");
+
+
+//map
+let myMap = new Map();
+myMap.set("age1", "20");
+myMap.set("age2", "30");
+console.log(myMap.get("age1")); // Output: value1
+console.log(myMap.get("age2")); // Output: value2
+
+//set
+let mySet = new Set();
+mySet.add(1);
+mySet.add(2);
+mySet.add(3);
+console.log(mySet.size); // Output: 3
+
+
