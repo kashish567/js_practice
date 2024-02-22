defcd59d8887fd1bab7d646cf21939e30ea23f51
kashish567 <kashishbhanushali7@gmail.com>
2024-02-22 20:11:54 +0530

    typecast

---
+++ typecast.js
@@ -0,0 +1,13 @@
+
+//Implicit type conversion
+let num = 10; //number
+let str = "20"; //string
+
+let result = num + str;
+console.log(result);
+
+//Explicit
+let stri = "10";
+let numb = parseInt(stri); // Explicit conversion from string to number
+console.log(numb); // Output: 10
+console.log(typeof numb); //number
