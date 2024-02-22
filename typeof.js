836dd3de107c8515b8deb93deb444764b9a11a82
kashish567 <kashishbhanushali7@gmail.com>
2024-02-22 20:04:57 +0530

    typeofoperators

---
+++ typeof.js
@@ -0,0 +1,13 @@
+console.log(typeof 42); // Output: "number"
+
+console.log(typeof "Hello"); // Output: "string"
+
+console.log(typeof true); // Output: "boolean"
+
+console.log(typeof undefined); // Output: "undefined"
+
+console.log(typeof null); // Output: "object" ( null is actually an object)
+
+console.log(typeof {}); // Output: "object"
+
+console.log(typeof function(){}); // Output: "function"
